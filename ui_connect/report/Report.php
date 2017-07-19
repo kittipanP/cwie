879fa79690bc974c7939fb551a886b5a09b65230
KITTIPAN <kittipan.prasertsang@gmail.com>
2017-07-19 18:45:44 +0700

    prepared report page

--- ui_connect/report/Report.php
+++ ui_connect/report/Report.php
@@ -8,87 +8,107 @@
             //All the meta and css links
             require_once '../../web_elements/head_link.php';?>
         <title>Report Management : WD Trainee Management</title>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
     </head>
+
+<style>
+body,h1 {font-family: "Raleway", sans-serif}
+body, html {height: 100%}
+.bgimg {
+    background-image: url('../../img/bg/my_passport_ssd.jpg');
+    min-height: 100%;
+    background-position: center;
+    background-size: cover;
+}
+</style>
 
 
 <body class="w3-theme-l5">
 
 <!-- Navbar -->
-<div class="w3-top">
+<!-- <div class="w3-top" > -->
  <!-- Navigation bar Code -->
-        <?php 
-            require_once '../../web_elements/nav_bar.php';?>
-<!-- Page Container -->
+         <?php require_once '../../web_elements/nav_bar.php';?>
+
+
+        <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+          <div class="w3-display-topleft w3-padding-large w3-xlarge">
+            Report
+          </div>
+          <div class="w3-display-middle">
+            <h1 class="w3-jumbo w3-animate-top">COMING SOON</h1>
+            <hr class="w3-border-grey" style="margin:auto;width:40%">
+            <p class="w3-large w3-center">R E P O R T &nbsp; M A N G E M E N T &nbsp; S Y S T E M</p>
+            <p class="w3-large w3-center">78,354,962  &nbsp; days left</p>
+          </div>
+        </div>
+
+
+
+</div> 
+
+ 
+
+</body>
+</html> 
+
+
+
+<!-- Page Container 
 <div class="w3-container w3-content" style="max-width:1400px;margin-top:80px">   
-	<!-- The Grid -->
-  	<div class="w3-row"> 
-		
+    <!-- The Grid 
+    <div class="w3-row"> 
+        
         <header class="w3-container w3-theme w3-padding w3-round"  id="myHeader">
                     <div class="w3-center">
                         <h2 class="w3-xxxlarge w3-animate-left">Welcome to WD Trainee</h2>
                         <h3 class="w3-xxlarge w3-animate-right">Report Management System</h3>
                     </div>
                 </header>
-                <p>&nbsp;</p>
-        
-        
-        
-        
-        
-        
-    <!-- Left Column -->
+                <p>&nbsp;</p>       
+    <!-- Left Column 
     <div class="w3-col m6">
-    	
-        <div class="w3-row-padding w3-center w3-margin-top">
         
+        <div class="w3-row-padding w3-center w3-margin-top">
             
-		</div>
+        </div>
    
-    <!-- End Left Column -->
+    <!-- End Left Column 
     </div>    
     
-	<!-- Right Column -->
+    <!-- Right Column 
     <div class="w3-col m6">
-     	<div class="w3-card-2 w3-round w3-white w3-padding-16 w3-center">
+        <div class="w3-card-2 w3-round w3-white w3-padding-16 w3-center">
         <p>Notification</p>
         <p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p><br></p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p><br></p>
 
       </div>
-    <!-- End Right Column -->
-    </div>
-    
-    
-    <!-- End The Grid -->
-    </div>
-    
-    
+      
+    <!-- End Right Column 
+    </div>  
+
+    <!-- End The Grid 
+    </div>   
 </div> 
   
-<!-- End Page Container -->
-</div>
-<p>&nbsp;</p>
-
-
-
-
-
-
- 
-
-</body>
-</html> 
+<!-- End Page Container 
+</div> -->
