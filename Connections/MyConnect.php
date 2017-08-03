f60ab95d5d219fcee21f672ef9a771ba889259c9
KITTIPAN <kittipan.prasertsang@gmail.com>
2017-08-03 13:35:25 +0700

    -

--- Connections/MyConnect.php
+++ Connections/MyConnect.php
@@ -11,10 +11,13 @@ mysqli_set_charset($MyConnect, "utf-8");
 //mysqli_query("Set Names UTF8");
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 mysqli_query($MyConnect, "SET character_set_results=utf8");
 mysqli_query($MyConnect, "SET character_set_client=utf8");
 mysqli_query($MyConnect, "SET character_set_connection=utf8"); 
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 $conn = new mysqli($hostname_MyConnect, $username_MyConnect, $password_MyConnect);
