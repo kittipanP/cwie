ff639d976934e77e1ba26d02e9fd13a6e2cf92ae
KITTIPAN <kittipan.prasertsang@gmail.com>
2017-08-09 09:44:09 +0700

    ubdated: cwie_db code

---
+++ ui_connect/student_management/insert/add-new-row/form-insert/add-major-form.php
@@ -0,0 +1,11 @@
+    
+
+    <form action="<?php echo $editFormAction; ?>" method="post" name="majorForm" id="fullform" enctype="multipart/form-data">
+
+
+
+
+        <input type="submit" name="submit" class="action-button" value="Submit" />
+        <input type="hidden" name="MM_insert" class="submit action-button" value="fullform" />
+        <input type="hidden" name="MM_insert" value="majorForm" />
+    </form>
