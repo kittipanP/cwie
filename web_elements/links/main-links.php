6e00547328821fe5aa4c16029da20a8a2d57ef12
kittipanP <kittipan.prasertsang@gmail.com>
2017-08-24 12:51:43 +0700

    creat */main-links.php

---
+++ web_elements/links/main-links.php
@@ -0,0 +1,65 @@
+
+
+<!-- icon logo-->
+<link rel="icon" type="image/png" href="../../img/images/wd.png"/>
+
+<!-- main stylesheet-->
+<link rel="stylesheet" href="../../libs/css/w3.css">
+<link rel="stylesheet" href="../../libs/css/w3-theme-blue-grey.css">
+<link rel='stylesheet' href='../../libs/css/css?family=Open+Sans'>
+
+<!-- Lato font -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+
+<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
+<!-- font-awesome-->
+<link rel="stylesheet" href="../../libs/css/font-awesome-4.7.0/css/font-awesome.min.css" type="text/css">
+<link href="../../libs/css/font-awesome.min.css" rel="stylesheet">
+
+
+      <!-- style muti-form-->
+      <link rel="stylesheet" href="../../libs/css/style-msform.css?v=0214i" type="text/css">
+
+      <!-- style full form-->
+      <link rel="stylesheet" href="../../libs/css/style-fullform.css?v=0214i" type="text/css">
+
+      <!-- style add form-->
+      <link rel="stylesheet" href="../../libs/css/style-addform.css?v=0214i" type="text/css">
+
+
+      <!-- According-->
+      <!--<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'>-->
+      <link rel="stylesheet" href="../../libs/css/style-according.css?v=0228i" type="text/css">
+
+
+	<!-- alert-sweetAlert2-->   
+    <link rel="stylesheet" href="../../libs/sweetAlert2/ajax-delete/assets/bootstrap/css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="../../libs/sweetAlert2/ajax-delete/assets/swal2/sweetalert2.min.css" type="text/css" />
+
+
+<!-- calendar-style -->
+<link rel="stylesheet" href="../../ui_connect/student_management/src/calendar.css">
+
+
+  <!-- customselect[]-->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+    <script src='../../libs/jQuery-Customselect/src/jquery-customselect.js'></script>
+    <link href='../../libs/jQuery-Customselect/src/jquery-customselect.css?v=<55' rel='stylesheet' />
+    
+    
+
+
+    <!-- Seach in Select-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.5.1/chosen.min.css" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.5.1/chosen.jquery.min.js"> </script>
+
+  <!-- bootstrap-select --> 
+  <link rel="stylesheet" href="../../libs/bootstrap-select/dist/css/bootstrap.min.css?v=<?php echo filemtime('bootstrap.min.css'); ?>"> 
+  <link rel="stylesheet" href="../../libs/bootstrap-select/dist/css/bootstrap-select.css?v=<?php echo filemtime('bootstrap-select.css'); ?>"> 
+  <script src="../../libs/bootstrap-select/dist/js/jquery.min.js"></script>
+  <script src="../../libs/bootstrap-select/dist/js/bootstrap.min.js"></script>
+  <script src="../../libs/bootstrap-select/dist/js/bootstrap-select.js"></script> 
+
+  
