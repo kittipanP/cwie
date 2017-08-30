51159b933bc5103d6e01e4741a9fca5cc715d63b
kittipanP <kittipan.prasertsang@gmail.com>
2017-08-30 11:03:20 +0700

    fixed: duration can't insert

--- ui_connect/student_management/insert/stu-insert-all/tabII.php
+++ ui_connect/student_management/insert/stu-insert-all/tabII.php
@@ -191,12 +191,12 @@
                         <div align="left">
                         <label for="">Starting Date: </label>
                         </div>
-                        <input type="text" name="" value="" size="32" placeholder="trigger calendar" id="dtwdS"/>
+                        <input type="text" name="application_dateS" value="" size="32" placeholder="trigger calendar" id="dtwdS"/>
 
                         <div align="left">
                         <label for="">Ending Date: </label>
                         </div>
-                        <input type="text" name="" value="" size="32" placeholder="trigger calendar" id="dtwdE"/>
+                        <input type="text" name="application_dateE" value="" size="32" placeholder="trigger calendar" id="dtwdE"/>
 
 
 
