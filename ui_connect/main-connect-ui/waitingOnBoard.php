5b06c8eccaa2a8e32de38a94640285ea769d688c
kittipanP <kittipan.prasertsang@gmail.com>
2017-09-28 17:14:22 +0700

    added feature basic search to endedTrainee, onProcess, rejectedStudent, trainee and waintingOnBoard

--- ui_connect/main-connect-ui/waitingOnBoard.php
+++ ui_connect/main-connect-ui/waitingOnBoard.php
@@ -362,6 +362,31 @@ header{ background: url(../../img/head/headerv.jpg);}
       
     </script>
 
+    
+    <script>
+
+    function onProcessFnii() {
+      var input, filter, table, tr, td, i,j;
+      input = document.getElementById("onProcessiiInput");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("onProcessiiTable");
+      tr = table.getElementsByTagName("tr");
+      
+      
+        for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[2];
+        if (td) {
+          if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+          } else {
+          tr[i].style.display = "none";
+          }
+        }
+        }
+      
+    }
+  </script>
+
   
     
 <?php 
